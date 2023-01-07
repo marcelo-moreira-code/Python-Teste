a4c38ba0618ae96252ec9f1a6b4bc0840b1320e9
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2023-01-06 22:53:25 -0300

    Update aula21.py

--- aulas/aula21.py
+++ aulas/aula21.py
@@ -88,3 +88,11 @@ print(f'A fora vale {a}')
 #print(f'C fora vale {c}')'''
 
 ## Retornando Valores - return
+
+def somar(a=0,b=0,c=0):
+    s = a + b + c
+    print(f'A soma vale {s}')
+
+
+
+somar(3,3,5)
